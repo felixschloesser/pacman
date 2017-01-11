5fe9606cfb40b583ef5d582fe331acffde7462eb
Felix Schlösser <felix@belmail.net>
2017-01-11 12:37:41 +0100

    Add files via upload
    Added ghost and cleaned up the code.

--- pacman.c
+++ pacman.c
@@ -1,10 +1,24 @@
+/*
+ncurses (new curses) - From Wikpedia
+is a programming library providing an application programming
+interface (API) that allows the programmer to write text-based user interfaces
+in a terminal-independent manner.
+It is a toolkit for developing "GUI-like" application software
+that runs under a terminal emulator.
+It also optimizes screen changes, in order to reduce the latency experienced
+when using remote shells.
+
+READ ONLINE: http://site.ebrary.com/lib/alltitles/docDetail.action?docID=10278601
+*/
+
 #include <stdio.h>
-#include <ncurses.h>
+#include <ncurses.h> // Libary used for early rpg like rogue
 #include <stdlib.h>
 #include <time.h>
 
 #define X_SIZE 21
 #define Y_SIZE 7
+
 
 char maze[Y_SIZE][X_SIZE] = {
 	"#####################",
@@ -16,176 +30,261 @@ char maze[Y_SIZE][X_SIZE] = {
 	"#####################"
 };
 
-const char wall		= '#';	// Maybe add "Obstruction" structm but how?
+const char wall		= '#';	// Maybe add "Obstruction" structure but how?
 const char space	= ' ';	// Maybe add "Walkable" struct
-const char player	= '@';
+const char pacman	= '@';
+const char ghost	= '%';
 const char cookie	= '.';
 
 struct position {		// Player with coodrinates (Y, X)
 	int y;
 	int x;
-};
-struct position playerPos;
+}player, blinky;
 
 char direction = ' ';
 char newDirection = ' ';
 
 int points = 0;
 
+WINDOW *win;
+
 void drawMaze(void) {
 	int x, y;
 	for(y = 0; y < Y_SIZE; y++) {
 		for(x = 0; x < X_SIZE; x++) {
-			if ( (x == playerPos.x) && (y == playerPos.y) ) {
-				attron(COLOR_PAIR(2));
-					printw("%c", player);
-				attroff(COLOR_PAIR(2));
+
+			//Coloring the ghost
+			if ( (x == blinky.x) && (y == blinky.y) ) {
+				wattron(win,COLOR_PAIR(4));
+				wprintw(win,"%c", ghost);
+				wattroff(win,COLOR_PAIR(4));
 			}
-			else if (maze [y][x] == wall){	
-				attron(COLOR_PAIR(3));
-					printw(" ");
-				attroff(COLOR_PAIR(3));
+			//Coloring the player
+			else if ( (x == player.x) && (y == player.y) ) {
+				wattron(win,COLOR_PAIR(2));
+				wprintw(win,"%c", pacman);
+				wattroff(win,COLOR_PAIR(2));
 			}
-			else if (maze [y][x] == space){	
-				attron(COLOR_PAIR(1));
-					printw("%c", maze[y][x]);
-				attroff(COLOR_PAIR(1));
+			// Cookies
+			else if (maze [y][x] == cookie) {
+				wattron(win,COLOR_PAIR(1));
+				wprintw(win,"%c", maze[y][x]);
+				wattroff(win,COLOR_PAIR(1));
 			}
-			else if (maze [y][x] == cookie){	
-				attron(COLOR_PAIR(1));
-					printw("%c", maze[y][x]);
-				attroff(COLOR_PAIR(1));
+			//Coloring the walls of the maze
+			else if (maze [y][x] == wall) {
+				wattron(win,COLOR_PAIR(3));
+				wprintw(win," ");
+				wattroff(win,COLOR_PAIR(3));
+			}
+			//Coloring the spaces
+			else if (maze [y][x] == space) {
+				wattron(win,COLOR_PAIR(1));
+				wprintw(win,"%c", maze[y][x]);
+				wattroff(win,COLOR_PAIR(1));
 			}
 		}
-		printw("\n");
 	}
-	printw("'w', 'a', 's', 'd' to move. 'x' to Exit.");      
-}
-
-char tileInDirection (char d) {
-	if (d==' ') return ' ';
-	if (d == 'N') {
-		return maze[playerPos.y-1] [playerPos.x];
-	}
-	if (d == 'E') {
-		return maze[playerPos.y] [playerPos.x+1];
-	}
-	if (d == 'S') {
-		return maze[playerPos.y+1] [playerPos.x];
-	}
-	if (d == 'W') {
-		return maze[playerPos.y] [playerPos.x-1];
-	}
-	printw("Invalid [%c]",d);
-	return ' ';
+	wrefresh(win);
 }
 
 void eatCookie() {
-	if (maze[playerPos.y] [playerPos.x] == cookie) {
+	if (maze[player.y] [player.x] == cookie) {
 		points+= 10;
-		maze[playerPos.y] [playerPos.x] = space;	
-	}
-}
-
-void movePlayer(direction) {
-	
-	if (direction == ' ') {
-		return;
-	}
-	
-	eatCookie();
-		
-	if (tileInDirection(direction) != wall ) {	
-		switch(direction) {
-			case 'N': playerPos.y--; break;
-			case 'E': playerPos.x++; break;
-		    case 'S': playerPos.y++; break;
-		    case 'W': playerPos.x--; break;   
-		}
+		maze[player.y] [player.x] = space;
 	}
 }
 
 
-char playerDirection(char input) {
-	if (input == 'w') {
-		if (direction == ' ') {
-			direction = 'N';
-		}
-		else {
-			newDirection = 'N';
-		}
-	}	
-	else if (input == 'a') {
-		if (direction == ' ') {
-			direction = 'W';
-		}
-		else {
-			newDirection = 'W';
-		}
-	}	
-	else if (input == 's') {
-		if (direction == ' ') {
-			direction = 'S';
-		}
-		else {
-			newDirection = 'S';
+// Optaining the state of a tile in a specific direction
+char getTileInDirection (int sender, char d) {
+	if (d == ' ') return ' '; 		// No direction
+
+	else if (d == 'N') {					// North
+		if (! sender) {
+			return maze[player.y-1] [player.x];
+		} else {
+			return maze[blinky.y-1] [blinky.x];
 		}
 	}
-	else if (input == 'd') {
-		if (direction == ' ') {
-			direction = 'E';
+	else if (d == 'E') { 					// East
+		if (! sender) {
+			return maze[player.y] [player.x+1];
+		} else {
+			return maze[blinky.y] [blinky.x+1];
 		}
-		else {
-			newDirection = 'E';
+	}
+	else if (d == 'S') { 					// South
+		if (! sender) {
+			return maze[player.y+1] [player.x];
+		} else {
+			return maze[blinky.y+1] [blinky.x];
+		}
+	}
+	else if (d == 'W') { 					// West
+		if (! sender) {
+			return maze[player.y] [player.x-1];
+		} else {
+			return maze[blinky.y] [blinky.x-1];
 		}
 	}
 	else {
-		printw("Invalid input");
-	} 
+		wprintw(win,"Invalid [%c]",d);
+		return ' ';
+	}
 }
 
+// 0 for player 1 for ghost
+void moveTo(int sender,char direction) {
+
+	if (direction == ' ') {
+		return;
+	}
+	if (getTileInDirection(sender, direction) != wall ) {
+		if (sender == 0) {
+			eatCookie();
+			// moving Pacman
+			switch(direction) {
+				case 'N': player.y--; break;
+				case 'E': player.x++; break;
+			  case 'S': player.y++; break;
+			  case 'W': player.x--; break;
+			}
+		}
+		else if (sender == 1) {
+		// moving Blinky
+			switch(direction) {
+				case 'N': blinky.y--; break;
+				case 'E': blinky.x++; break;
+			  case 'S': blinky.y++; break;
+			  case 'W': blinky.x--; break;
+			}
+		}
+
+	}
+}
+
+void moveGhost() {
+	char randomDirection = "NESW"[random () % 4];
+	moveTo(1,randomDirection);
+	return;
+}
+
+void playerDirection(int input) {
+	switch(input) {
+		case 'w':
+		case KEY_UP:	if (direction == ' ') {
+							direction = 'N';
+						}
+						else {
+							newDirection = 'N';
+						}
+						break;
+
+		case 'a':
+		case KEY_LEFT:	if (direction == ' ') {
+							direction = 'W';
+						}
+						else {
+							newDirection = 'W';
+						}
+						break;
+
+		case 's':
+		case KEY_DOWN:	if (direction == ' ') {
+							direction = 'S';
+						}
+						else {
+							newDirection = 'S';
+						}
+						break;
+
+		case 'd':
+		case KEY_RIGHT:	if (direction == ' ') {
+							direction = 'E';
+						}
+						else {
+							newDirection = 'E';
+						}
+						break;
+	}
+}
+
+
 int main(void) {
+
+	int starty,startx;
+  int input;
+
 	initscr();
 	raw();
-	//cbreak();
-	noecho();
-	timeout(0);
-	
-	start_color();	
-	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(3, COLOR_BLUE, COLOR_BLUE);
 
-	playerPos.y = 5;
-	playerPos.x = 10;
-	
-	int c = 0;
-	int k = 0;
+	noecho();
+	keypad(stdscr, TRUE);
+
+	//Radom generator initialazation
+	srand(time(NULL));
+	//Initializeing the colors
+	start_color();
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);		// Cookies
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK);	// Packman / Player
+	init_pair(3, COLOR_BLUE, COLOR_BLUE);			// Walls
+	init_pair(4, COLOR_RED, COLOR_BLACK);			// Blinky
+
+
+	starty = (LINES - Y_SIZE) / 2;	// finding the middle of the window
+	startx = (COLS - X_SIZE) / 2;		// same
+
+	printw("'w', 'a', 's', 'd' to move. 'x' to Exit");
+	refresh();
+
+	win = newwin(Y_SIZE, X_SIZE, starty, startx);
+
+	// Player starting Position
+	player.y = 5;
+	player.x = 10;
+
+	// Ghost starting Position
+	blinky.y = 1;
+	blinky.x = 10;
+
+	// Timing / Ticks
+	timeout(0);
+	int ticks = 0;
+
 	drawMaze();
-	
-	char input;
+
 	do {
 		input = getch();
-		if ((input=='w') || (input=='a') || (input=='s') || (input=='d')) {
-	    	playerDirection(input);
+	  playerDirection(input);
+
+	  if ( (ticks % 16) == 0) {		// evey tenth loop redraws the maze
+			werase(win);
+			moveGhost();
+	    moveTo(0, direction); // 0 for player as the sender of
+
+			if ((newDirection != ' ') && (getTileInDirection(0, newDirection) != wall)) {
+	    	direction = newDirection;
+	    	newDirection = ' ';
 	    }
-	    if ( (c % 16) == 0) {		// evry tenth loop redraw the maze.
-	    	erase();	
-	    	movePlayer(direction);
-	    	if ((newDirection != ' ') && (tileInDirection(newDirection) != wall)) {
-	    		direction = newDirection;
-	    		newDirection = ' ';
-	    	}
-	    	printw("%d", points);
-	   		drawMaze();
-	    }
-	    
-	   	usleep(10000);
-	   			
-		// printw("%c",input);
-		c++;
+	    mvprintw(2,2,"%d", points);
+
+			//GAME OVER routine
+			if ( (blinky.x == player.x) && (blinky.y == player.y) ) {
+				mvprintw(starty-2, startx, "GAME OVER");
+				drawMaze();
+				usleep(1500000);
+				endwin();
+				return 0;
+			}
+			drawMaze();
+		}
+
+	  usleep(10000);
+
+		ticks++;
 	} while(input != 'x');
-	
+
 	endwin();
 	return 0;
 }
