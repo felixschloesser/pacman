af8aebb40855cbe8e0e9d4c76113ffea11e3e6d4
Felix Schlösser <felix@belmail.net>
2017-01-19 16:53:37 +0100

    Add files via upload

--- pacman.c
+++ pacman.c
@@ -11,11 +11,13 @@ when using remote shells.
 READ ONLINE: http://site.ebrary.com/lib/alltitles/docDetail.action?docID=10278601
 */
 #include <stdio.h>
-#include <ncurses.h> // Libary used for early rpg like rogue
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
+#include <math.h>
+
+#include <ncurses.h> // Libary used for early rpg like rogue
+#include <unistd.h> // macPort of ncruses
 
 #define MAXMAZE 1000
 
@@ -28,9 +30,9 @@ const char c_cookie	= '.';
 struct figure {		// Player / ghosts with coodrinates (Y, X), and direction
 	int y;
 	int x;
-	char nowDirection;
-	char newDirection;
-} player, ghost,blinky;
+	char direction;
+	char nextDirection;
+} player, ghost, blinky;
 
 char maze[MAXMAZE];
 int sizex=0, sizey = 0;
@@ -115,94 +117,266 @@ void drawMaze(void) {
 	wrefresh(win);
 }
 
-void eatCookie(struct figure *f) {
-	if (maze_get(f->x, f->y) == c_cookie) {
+void eatCookie(struct figure *player) {
+	if (maze_get(player->x, player->y) == c_cookie) {
 		points += 10;
-		maze_put(f->x, f->y, c_space);
+		maze_put(player->x, player->y, c_space);
 	}
 }
 
-char getTileInDirection (char d,struct figure *f) {
+char getTileInDirection (char d, struct figure *figure) {
 	switch(d) {
 		case ' ': return ' '; break; // No direction
-		case 'N': return maze_get(f->x,f->y-1); break;
-		case 'E': return maze_get(f->x+1,f->y); break;
-		case 'S': return maze_get(f->x,f->y+1); break;
-		case 'W': return maze_get(f->x-1,f->y); break;
+		case 'N': return maze_get(figure->x,figure->y-1); break;
+		case 'E': return maze_get(figure->x+1,figure->y); break;
+		case 'S': return maze_get(figure->x,figure->y+1); break;
+		case 'W': return maze_get(figure->x-1,figure->y); break;
 	 //	default: wprintw(win,"Invalid [%c]",d); break;
 	}
 	return ' ';
 }
 
-void moveFigure(struct figure *f) {
+void moveFigure(struct figure *figure) {
 
-	if (f->nowDirection == ' ') {
+	if (figure->direction == ' ') {
 		return;
 	}
-	if (getTileInDirection(f->nowDirection,f) != c_wall ) {
-		switch(f->nowDirection) {
-			case 'N': f->y--; break;
-			case 'E': f->x++; break;
-			case 'S': f->y++; break;
-			case 'W': f->x--; break;
+	if (getTileInDirection(figure->direction,figure) != c_wall ) {
+		switch(figure->direction) {
+			case 'N': figure->y--; break;
+			case 'E': figure->x++; break;
+			case 'S': figure->y++; break;
+			case 'W': figure->x--; break;
 		}
 	}
 }
 
-void setDirectionRandom(struct figure *f) {
+void setDirectionRandom(struct figure *ghost) {
 	char randomDirection = "NESW"[random () % 4];
-	f->nowDirection = randomDirection;
+	ghost->direction = randomDirection;
 }
 
-void setDirectionFromInput(int input,struct figure *f) {
+void addValue(char *arr, int *len, char value) {
+	arr[*len] = value;
+    ++*len;
+}
+
+void rmValue(char *arr, int *len, char value){
+	int i = 0;
+	int pos = -1;
+	if (*len > 0) {
+		for(i = 0; i < *len; i++) {
+	  	if (arr[i] == value) {
+				pos = i;
+			}
+		}
+	}
+  if (pos > -1) {
+ 		if(pos+1 == *len) {
+			*len--;
+			return;
+		}
+		for(i = pos; i < *len-1; i++)
+		arr[i] = arr[i+1];
+		*len--;
+	}
+}
+
+char sourceDirection(char d) {
+  switch(d) {
+		case ' ': return ' '; break; // No direction
+		case 'N': return 'S'; break;
+		case 'E': return 'W'; break;
+		case 'S': return 'N'; break;
+		case 'W': return 'E'; break;
+	 	//	default: wprintw(win,"Invalid [%c]",d); break;
+	}
+	return ' ';
+}
+
+void blinkyAI(struct figure *figure) {
+	char branches[4];
+	int numOfBranches = 0;
+  int d = 0;
+  struct figure f1,f2;
+
+  for(d = 0; d < 4; d++)
+		if (getTileInDirection("NESW"[d], figure) != c_wall) {
+			addValue(branches,&numOfBranches,"NESW"[d]);
+		}
+		if ((figure->direction != ' ') && (numOfBranches > 1)) {
+			rmValue(branches,&numOfBranches,sourceDirection(figure->direction));
+		}
+		switch (numOfBranches) {
+	 		case 1:	figure->direction = branches[0];
+							break;
+
+			case 2:	f1 = *figure; f1.direction = branches[0];
+							moveFigure(&f1);
+
+							f2 = *figure; f2.direction = branches[1];
+							moveFigure(&f2);
+
+							if (sqrt( pow(f1.x - player.x, 2) + pow(f1.y - player.y, 2)) >
+	 	    					sqrt( pow(f2.x - player.x, 2) + pow(f2.y - player.y, 2))) {
+									figure->direction =   branches[1];
+							}
+	 						else {
+								figure->direction =   branches[0];
+							}
+	 						break;
+
+			case 4:	break;	// open space, keep direction
+
+	}
+}
+
+
+void ghostAI(struct figure *ghost) {
+
+	int numOfBranches = 0;
+	float distanceToPlayer;
+	char direction;
+
+	if (getTileInDirection('N', ghost) != c_wall)
+	 	numOfBranches++;
+	if (getTileInDirection('E', ghost) != c_wall)
+	 	numOfBranches++;
+	if (getTileInDirection('S', ghost) != c_wall)
+		numOfBranches++;
+	if (getTileInDirection('W', ghost) != c_wall)
+		numOfBranches++;
+
+
+	 switch (numOfBranches) {			//dead end -> turn other way
+	 	case 1: if (ghost->direction == 'N')
+							ghost->direction = 'S';
+						if (ghost->direction == 'E')
+							ghost->direction = 'W';
+						if (ghost->direction == 'S')
+							ghost->direction = 'N';
+						if (ghost->direction == 'W')
+							ghost->direction = 'E';
+						break;
+
+		// A TURN OR A STRAIGHT
+		case 2:	if (getTileInDirection('W', ghost) != c_wall) {
+
+							if (getTileInDirection('N', ghost) != c_wall) {		// West - North turn
+								if (ghost->direction == 'E') // going to the West
+									ghost->direction = 'N';
+
+								if (ghost->direction == 'S')	// going to the North
+									ghost->direction = 'W';
+							}
+							if (getTileInDirection('S', ghost) != c_wall) {		// West - South turn
+								if (ghost->direction == 'E') // going to the West
+									ghost->direction = 'S';
+
+								if (ghost->direction == 'N')	// going to the South
+									ghost->direction = 'W';
+							}
+						}
+
+						if (getTileInDirection('E', ghost) != c_wall) {
+
+							if (getTileInDirection('N',ghost) != c_wall) {			// East - North turn
+								if (ghost->direction == 'W') // going to the East
+									ghost->direction = 'N';
+
+								if (ghost->direction == 'S')	// going to the North
+									ghost->direction = 'E';
+								}
+							if (getTileInDirection('S',ghost) != c_wall) {		// East - South turn
+								if (ghost->direction == 'W') // going to the East
+									ghost->direction = 'S';
+
+								if (ghost->direction == 'N')	// going to the South
+									ghost->direction = 'E';
+							}
+						}
+						break;
+
+		// MORE THAN THREE BRANCHES
+		case 3:
+		case 4:	if (getTileInDirection('N', ghost) != c_wall)	{
+							distanceToPlayer =  sqrt( pow(ghost->x - player.x, 2) + pow(ghost->y+1 - player.y, 2));
+							direction = 'N';
+						}
+
+						if (getTileInDirection('E', ghost) != c_wall)	{
+							if (distanceToPlayer <= sqrt( pow(ghost->x-1 - player.x, 2) + pow(ghost->y - player.y, 2)) ) {
+								distanceToPlayer =  sqrt( pow(ghost->x-1 - player.x, 2) + pow(ghost->y - player.y, 2));
+								direction = 'E';
+							}
+						}
+						if (getTileInDirection('S', ghost) != c_wall)	{
+							if (distanceToPlayer <= sqrt( pow(ghost->x - player.x, 2) + pow(ghost->y-1 - player.y, 2)) ){
+								distanceToPlayer = sqrt( pow(ghost->x - player.x, 2) + pow(ghost->y-1 - player.y, 2));
+								direction = 'S';
+							}
+						}
+						if (getTileInDirection('W', ghost) != c_wall)	{
+							if (distanceToPlayer <= sqrt( pow(ghost->x-1 - player.x , 2) + pow(ghost->y - player.y, 2)) ){
+								distanceToPlayer = sqrt( pow(ghost->x-1 - player.x , 2) + pow(ghost->y - player.y, 2));
+								direction = 'W';
+							}
+						}
+						//printf("Distance %f", distanceToPlayer);
+						ghost->direction = direction;
+						break;
+	}
+}
+
+void setDirectionFromInput(int input,struct figure *player) {
 	switch(input) {
 		case 'w':
-		case KEY_UP:	if (f->nowDirection == ' ') {
-							f->nowDirection = 'N';
-						}
-						else {
-							f->newDirection = 'N';
-						}
-						break;
-
+		case KEY_UP:	if (player->direction == ' ') {
+										player->direction = 'N';
+									}
+									else {
+										player->nextDirection = 'N';
+									}
+									break;
 		case 'a':
-		case KEY_LEFT:	if (f->nowDirection == ' ') {
-							f->nowDirection = 'W';
-						}
-						else {
-							f->newDirection = 'W';
-						}
-						break;
-
+		case KEY_LEFT:	if (player->direction == ' ') {
+											player->direction = 'W';
+										}
+										else {
+											player->nextDirection = 'W';
+										}
+										break;
 		case 's':
-		case KEY_DOWN:	if (f->nowDirection == ' ') {
-						    f->nowDirection = 'S';
-						}
-						else {
-							f->newDirection = 'S';
-						}
-						break;
-
+		case KEY_DOWN:	if (player->direction == ' ') {
+											player->direction = 'S';
+										}
+										else {
+											player->nextDirection = 'S';
+										}
+										break;
 		case 'd':
-		case KEY_RIGHT:	if (f->nowDirection == ' ') {
-							f->nowDirection = 'E';
-						}
-						else {
-							f->newDirection = 'E';
-						}
-						break;
+		case KEY_RIGHT:	if (player->direction == ' ') {
+											player->direction = 'E';
+										}
+										else {
+											player->nextDirection = 'E';
+										}
+										break;
 	}
 }
 
-void setNewDirection(struct figure *f)
+
+
+void setnextDirection(struct figure *player)
 {
-	if (f->newDirection != ' ') {
-		if (getTileInDirection(player.newDirection,&player) != c_wall) {
-			player.nowDirection = player.newDirection;
-			player.newDirection = ' ';
+	if (player->nextDirection != ' ') {
+		if (getTileInDirection(player->nextDirection,player) != c_wall) {
+			player->direction = player->nextDirection;
+			player->nextDirection = ' ';
 		}
 	}
 }
+
 
 void printPoints (void) {
 	mvprintw(2,2,"%4d", points);
@@ -214,8 +388,7 @@ void debug (void) {
 }
 
 int checkIfGameOver(int input) {
-
-	if (input == 'x') {
+	if ((input == 'x') || (input == 'q')) {
 		return 10;
 	}
 	if (points > 350) {
@@ -233,7 +406,6 @@ int checkIfGameOver(int input) {
 	}
 	return 0;
 }
-
 
 int main(void) {
 	int starty,startx;
@@ -272,9 +444,11 @@ int main(void) {
 	// Ghost starting Position
 	ghost.y = 1;
 	ghost.x = 10;
-	// Ghost starting Position
+    ghost.direction = ' ';
+
 	blinky.y = 1;
 	blinky.x = 11;
+	blinky.direction = ' ';
 
 	// Timing / Ticks
 	int ticks = 0;
@@ -287,22 +461,24 @@ int main(void) {
 	  	if (input != 255) {
 			setDirectionFromInput(input,&player);
 		}
-		if ((ticks % 16) == 0) {		// evey tenth loop redraws the maze
-			setDirectionRandom(&ghost);
-      moveFigure(&ghost);
+		if ((ticks % 30) == 0) {		// evey tenth loop redraws the maze
+			//setDirectionRandom(&ghost);
+			ghostAI(&ghost);
+    		moveFigure(&ghost);
 
-			setDirectionRandom(&blinky);
-      moveFigure(&blinky);
-
+			blinkyAI(&blinky);
+        	moveFigure(&blinky);
+	  }
+	  if ((ticks % 16) == 0) {		// evey tenth loop redraws the maze;
 			moveFigure(&player);
 			eatCookie(&player);
-			setNewDirection(&player);
+			setnextDirection(&player);
 
 			printPoints();
 			drawMaze();
 	  }
 		if ((ticks % 100) == 0) {
-			points = points - 5;
+			//points = points - 5;
 			printPoints();
 		}
 		gameState = checkIfGameOver(input);
