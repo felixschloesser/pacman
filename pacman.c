b5701c85eab6b9d45fabe157b9c6933ee6253f52
Felix Schlösser <felix@belmail.net>
2017-01-21 19:38:33 +0100

    New maze, better AI, more Ghosts, teleportation.

--- pacman.c
+++ pacman.c
@@ -1,47 +1,50 @@
-/*
-ncurses (new curses) - From Wikpedia
-is a programming library providing an application programming
-interface (API) that allows the programmer to write text-based user interfaces
-in a terminal-independent manner.
-It is a toolkit for developing "GUI-like" application software
-that runs under a terminal emulator.
-It also optimizes screen changes, in order to reduce the latency experienced
-when using remote shells.
+//
+//  pacman.c
+//  Procedural Programming Project @ Hamburg University of Technology
+//
+//  Created by Shayan Bahadori Rad, George Bahna and Felix Schlösser on 16.12.16.
+//  Copyright © 2017 Shayan Bahadori Rad, George Bahna and Felix Schlösser. All rights reserved.
+//
 
-READ ONLINE: http://site.ebrary.com/lib/alltitles/docDetail.action?docID=10278601
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
 
-#include <ncurses.h> // Libary used for early rpg like rogue
-#include <unistd.h> // macPort of ncruses
+#include <ncurses.h>	// Libary used for early rpg like rogue
+#include <unistd.h>		// macOS port of ncruses
 
-#define MAXMAZE 1000
+#define MAXMAZE 1024
 
-const char c_wall		= '#';
+const char c_wall	= '#';
 const char c_space	= ' ';
 const char c_player	= '@';
 const char c_ghost	= '%';
 const char c_cookie	= '.';
+const char c_border	= 'B';
+const char c_cake	= 'o';
 
 struct figure {		// Player / ghosts with coodrinates (Y, X), and direction
 	int y;
 	int x;
 	char direction;
 	char nextDirection;
-} player, ghost, blinky;
+	int  state;  // sec || 0=normal / agressive, >4 stupid, >10still, >20frightend
+} player, blinky, pinky, bashful;
 
 char maze[MAXMAZE];
 int sizex=0, sizey = 0;
 
-int points = 100;
+int cookies = 0;
+int seconds = 0;
 
 WINDOW *win;
 
 char maze_get(int x, int y) { // x=0..sizex-1,
+	if (x<0 || y<0 || x>=sizex || y>=sizey) {
+	return c_border;
+	}
 	return maze[x + y*sizex];
 }
 
@@ -49,23 +52,46 @@ void maze_put(int x, int y, char c) {
 	maze[x + y*sizex] = c;
 }
 
+int countNumOfCookies() {
+	int i;
+	int numOfCookies = 0;
+
+	for(i = 0; i < MAXMAZE; i++) {
+		if (maze[i] == '.') {
+			numOfCookies++;
+		}
+	}
+	return numOfCookies;
+}
+
 void load_maze() {
-  sizex = 0; sizey = 0;
-  strcpy(maze,"#####################");
-  sizex = 21;
-  sizey++;
-  strcat(maze,"#......#     #      #");
-  sizey++;
-  strcat(maze,"#.##.#.### ###.#.## #");
-  sizey++;
-  strcat(maze,"#.#..#.........#..# #");
-  sizey++;
-  strcat(maze,"#.##.#.#######.# ## #");
-  sizey++;
-  strcat(maze,"#........           #");
-  sizey++;
-  strcat(maze,"#####################");
-  sizey++;
+  sizey = 0;
+	sizex = 23;
+
+	strcpy(maze,"#######################"); sizey++;
+  strcat(maze,"#..........#..........#");	sizey++;
+  strcat(maze,"#.###.####.#.####.###.#");	sizey++;
+  strcat(maze,"#o# #.#  #.#.#  #.# #o#");	sizey++;
+  strcat(maze,"#.###.####.#.####.###.#");	sizey++;
+  strcat(maze,"#.....................#");	sizey++;
+  strcat(maze,"#.###.#.#######.#.###.#");	sizey++;
+	strcat(maze,"#.....#....#....#.....#");	sizey++;
+	strcat(maze,"#####.#.##.#.##.#.#####");	sizey++;
+	strcat(maze,"    #.#         #.#    ");	sizey++;
+	strcat(maze,"#####.# ### ### #.#####");	sizey++;
+	strcat(maze,"     .  #     #  .     ");	sizey++;
+	strcat(maze,"#####.#.#######.#.#####");	sizey++;
+	strcat(maze,"    #.#         #.#    ");	sizey++;
+	strcat(maze,"#####.# ####### #.#####");	sizey++;
+	strcat(maze,"#..........#..........#");	sizey++;
+	strcat(maze,"#.###.####.#.####.###.#");	sizey++;
+	strcat(maze,"#o..#.............#..o#");	sizey++;
+	strcat(maze,"###.#.#.#######.#.#.###");	sizey++;
+	strcat(maze,"#.....#....#....#.....#");	sizey++;
+	strcat(maze,"#.########.#.########.#");	sizey++;
+	strcat(maze,"#.....................#");	sizey++;
+	strcat(maze,"#######################");	sizey++;
+
 }
 
 void drawMaze(void) {
@@ -76,16 +102,40 @@ void drawMaze(void) {
 		for(x = 0; x < sizex; x++) {
 
 			//Coloring the ghosts
-			if ( (x == ghost.x) && (y == ghost.y) ) {
+			if ( (x == pinky.x) && (y == pinky.y) ) {
 				wattron(win,COLOR_PAIR(4));
+				if (pinky.state > 0) {
+					wattron(win,A_BLINK);
+				}
 				wprintw(win,"%c", c_ghost);
+				if (pinky.state > 0) {
+					wattroff(win,A_BLINK);
+				}
 				wattroff(win,COLOR_PAIR(4));
 			}
 			else if ( (x == blinky.x) && (y == blinky.y) ) {
 				wattron(win,COLOR_PAIR(5));
+				if (blinky.state > 0) {
+					wattron(win,A_BLINK);
+				}
 				wprintw(win,"%c", c_ghost);
+				if (blinky.state > 0) {
+					wattroff(win,A_BLINK);
+				}
 				wattroff(win,COLOR_PAIR(5));
 			}
+			else if ( (x == bashful.x) && (y == bashful.y) ) {
+				wattron(win,COLOR_PAIR(6));
+				if (blinky.state > 0) {
+					wattron(win,A_BLINK);
+				}
+				wprintw(win,"%c", c_ghost);
+				if (blinky.state > 0) {
+					wattroff(win,A_BLINK);
+				}
+				wattroff(win,COLOR_PAIR(6));
+			}
+
 
 			//Coloring the player
 			else if ( (x == player.x) && (y == player.y) ) {
@@ -97,6 +147,12 @@ void drawMaze(void) {
 			else if (maze_get(x,y) == c_cookie) {
 				wattron(win,COLOR_PAIR(1));
 				wprintw(win,"%c", c_cookie);
+				wattroff(win,COLOR_PAIR(1));
+			}
+			// Cake
+			else if (maze_get(x,y) == c_cake) {
+				wattron(win,COLOR_PAIR(1));
+				wprintw(win,"%c", c_cake);
 				wattroff(win,COLOR_PAIR(1));
 			}
 			//Coloring the walls of the maze
@@ -112,14 +168,24 @@ void drawMaze(void) {
 				wattroff(win,COLOR_PAIR(1));
 			}
 		}
-		//wprintw(win,"\n");
 	}
 	wrefresh(win);
 }
 
 void eatCookie(struct figure *player) {
-	if (maze_get(player->x, player->y) == c_cookie) {
-		points += 10;
+	if (maze_get(player->x, player->y) == c_cake) {
+		cookies -= 1;
+
+		blinky.state += 12;
+		pinky.state += 16;
+
+		pinky.y = 11;
+		pinky.x = 10;
+
+		maze_put(player->x, player->y, c_space);
+	}
+	else if (maze_get(player->x, player->y) == c_cookie) {
+		cookies -= 1;
 		maze_put(player->x, player->y, c_space);
 	}
 }
@@ -131,17 +197,23 @@ char getTileInDirection (char d, struct figure *figure) {
 		case 'E': return maze_get(figure->x+1,figure->y); break;
 		case 'S': return maze_get(figure->x,figure->y+1); break;
 		case 'W': return maze_get(figure->x-1,figure->y); break;
-	 //	default: wprintw(win,"Invalid [%c]",d); break;
 	}
 	return ' ';
 }
 
 void moveFigure(struct figure *figure) {
-
 	if (figure->direction == ' ') {
 		return;
 	}
-	if (getTileInDirection(figure->direction,figure) != c_wall ) {
+	char c;
+	c = getTileInDirection(figure->direction,figure);
+	if (c == c_border) {  // teleport to the other side of the maze
+		if (figure->x==0) 	{figure->x=sizex-1;}
+      else if (figure->x==sizex-1) {figure->x=0;}
+      else if (figure->y==0) 	{figure->y=sizey-1;}
+      else if (figure->y==sizey-1) {figure->y=0;}
+	}
+	else if (c != c_wall ) {
 		switch(figure->direction) {
 			case 'N': figure->y--; break;
 			case 'E': figure->x++; break;
@@ -151,177 +223,78 @@ void moveFigure(struct figure *figure) {
 	}
 }
 
-void setDirectionRandom(struct figure *ghost) {
+void stupidAI(struct figure *figure) {
 	char randomDirection = "NESW"[random () % 4];
-	ghost->direction = randomDirection;
+	figure->direction = randomDirection;
 }
 
 void addValue(char *arr, int *len, char value) {
 	arr[*len] = value;
-    ++*len;
+  	++*len;
 }
 
-void rmValue(char *arr, int *len, char value){
+void rmValue(char *array, int *length, char value){
 	int i = 0;
-	int pos = -1;
-	if (*len > 0)
-	{
-		for(i = 0; i < *len; i++)
-		{
-	  		if (arr[i] == value)
-			{pos = i;}
+	int position = -1;
+	if (*length > 0) {
+		for(i = 0; i < *length; i++) {
+	  	if (array[i] == value) {
+				position = i;
+			}
 		}
 	}
-    if (pos > -1)
-    {
- 		if(pos+1==*len){
-			--*len;
+  if (position > -1) {
+ 		if(position+1 == *length) {
+			--*length;
 			return;
 		}
-		for(i = pos; i < *len-1; i++)
-		arr[i] = arr[i+1];
-		--*len;
+		for(i = position; i < *length -1; i++) {
+			array[i] = array[i+1];
+			--*length;
+		}
 	}
 }
 
-char sourceDirection(char d) {
-   	switch(d) {
+char oppositeDirection(char d) {
+	switch(d) {
 		case ' ': return ' '; break; // No direction
 		case 'N': return 'S'; break;
 		case 'E': return 'W'; break;
 		case 'S': return 'N'; break;
 		case 'W': return 'E'; break;
-	 //	default: wprintw(win,"Invalid [%c]",d); break;
 	}
-	return ' ';
+	return ' '; //default
 }
 
-void blinkyAI(struct figure *figure) {
+void ghostAI(struct figure *figure) {
 	char branches[4];
 	int numOfBranches = 0;
-    int d = 0;
-    struct figure f1,f2;
+  int d = 0;
+  struct figure f1,f2;
+  char map[] = "NESW";
 
-  	for(d = 0; d < 4; d++)
-	if (getTileInDirection("NESW"[d], figure) != c_wall)
-	 	{addValue(branches,&numOfBranches,"NESW"[d]);}
-	if ((figure->direction != ' ') && (numOfBranches > 1))
-	{rmValue(branches,&numOfBranches,sourceDirection(figure->direction));}
-
-	 switch (numOfBranches) {
-	 	case 1:
-	 			figure->direction = branches[0];
-				break;
-		case 2:
-		   f1 = *figure; f1.direction = branches[0];
-		   moveFigure(&f1);
-		   f2 = *figure; f2.direction = branches[1];
-		   moveFigure(&f2);
-	 	  if (sqrt( pow(f1.x - player.x, 2) + pow(f1.y - player.y, 2)) >
-	 	     sqrt( pow(f2.x - player.x, 2) + pow(f2.y - player.y, 2)))
-	 	  {figure->direction =   branches[1];}
-	 	  else
-	 	  {figure->direction =   branches[0];}
-	 	  break;
-		case 4:  // open space, keep direction
-		break;
+  for(d = 0; d < 4; d++) {
+		if (getTileInDirection(map[d], figure) != c_wall) {
+			addValue(branches,&numOfBranches,map[d]);
+		}
+		if ((figure->direction != ' ') && (numOfBranches > 1))	{
+			rmValue(branches,&numOfBranches,oppositeDirection(figure->direction));
+		}
 	}
-}
-
-
-void ghostAI(struct figure *ghost) {
-
-	int numOfBranches = 0;
-	float distanceToPlayer;
-	char direction;
-
-	if (getTileInDirection('N', ghost) != c_wall)
-	 	numOfBranches++;
-	if (getTileInDirection('E', ghost) != c_wall)
-	 	numOfBranches++;
-	if (getTileInDirection('S', ghost) != c_wall)
-		numOfBranches++;
-	if (getTileInDirection('W', ghost) != c_wall)
-		numOfBranches++;
-
-
-	 switch (numOfBranches) {			//dead end -> turn other way
-	 	case 1: if (ghost->direction == 'N')
-							ghost->direction = 'S';
-						if (ghost->direction == 'E')
-							ghost->direction = 'W';
-						if (ghost->direction == 'S')
-							ghost->direction = 'N';
-						if (ghost->direction == 'W')
-							ghost->direction = 'E';
-						break;
-
-		// A TURN OR A STRAIGHT
-		case 2:	if (getTileInDirection('W', ghost) != c_wall) {
-
-							if (getTileInDirection('N', ghost) != c_wall) {		// West - North turn
-								if (ghost->direction == 'E') // going to the West
-									ghost->direction = 'N';
-
-								if (ghost->direction == 'S')	// going to the North
-									ghost->direction = 'W';
-							}
-							if (getTileInDirection('S', ghost) != c_wall) {		// West - South turn
-								if (ghost->direction == 'E') // going to the West
-									ghost->direction = 'S';
-
-								if (ghost->direction == 'N')	// going to the South
-									ghost->direction = 'W';
-							}
-						}
-
-						if (getTileInDirection('E', ghost) != c_wall) {
-
-							if (getTileInDirection('N',ghost) != c_wall) {			// East - North turn
-								if (ghost->direction == 'W') // going to the East
-									ghost->direction = 'N';
-
-								if (ghost->direction == 'S')	// going to the North
-									ghost->direction = 'E';
-								}
-							if (getTileInDirection('S',ghost) != c_wall) {		// East - South turn
-								if (ghost->direction == 'W') // going to the East
-									ghost->direction = 'S';
-
-								if (ghost->direction == 'N')	// going to the South
-									ghost->direction = 'E';
-							}
-						}
-						break;
-
-		// MORE THAN THREE BRANCHES
-		case 3:
-		case 4:	if (getTileInDirection('N', ghost) != c_wall)	{
-							distanceToPlayer =  sqrt( pow(ghost->x - player.x, 2) + pow(ghost->y+1 - player.y, 2));
-							direction = 'N';
-						}
-
-						if (getTileInDirection('E', ghost) != c_wall)	{
-							if (distanceToPlayer <= sqrt( pow(ghost->x-1 - player.x, 2) + pow(ghost->y - player.y, 2)) ) {
-								distanceToPlayer =  sqrt( pow(ghost->x-1 - player.x, 2) + pow(ghost->y - player.y, 2));
-								direction = 'E';
-							}
-						}
-						if (getTileInDirection('S', ghost) != c_wall)	{
-							if (distanceToPlayer <= sqrt( pow(ghost->x - player.x, 2) + pow(ghost->y-1 - player.y, 2)) ){
-								distanceToPlayer = sqrt( pow(ghost->x - player.x, 2) + pow(ghost->y-1 - player.y, 2));
-								direction = 'S';
-							}
-						}
-						if (getTileInDirection('W', ghost) != c_wall)	{
-							if (distanceToPlayer <= sqrt( pow(ghost->x-1 - player.x , 2) + pow(ghost->y - player.y, 2)) ){
-								distanceToPlayer = sqrt( pow(ghost->x-1 - player.x , 2) + pow(ghost->y - player.y, 2));
-								direction = 'W';
-							}
-						}
-						//printf("Distance %f", distanceToPlayer);
-						ghost->direction = direction;
-						break;
+	if (numOfBranches == 1) {
+		figure->direction = branches[0];}
+	else {
+		f1 = *figure; f1.direction = branches[0];
+		moveFigure(&f1);
+		f2 = *figure; f2.direction = branches[1];
+		moveFigure(&f2);
+		if ( sqrt( pow(f1.x - player.x, 2) + pow(f1.y - player.y, 2)) >
+		 sqrt( pow(f2.x - player.x, 2) + pow(f2.y - player.y, 2)) ) {
+			 figure->direction =   branches[1];
+		}
+		else {
+			figure->direction =   branches[0];
+		}
 	}
 }
 
@@ -363,9 +336,7 @@ void setDirectionFromInput(int input,struct figure *player) {
 }
 
 
-
-void setnextDirection(struct figure *player)
-{
+void setnextDirection(struct figure *player) {
 	if (player->nextDirection != ' ') {
 		if (getTileInDirection(player->nextDirection,player) != c_wall) {
 			player->direction = player->nextDirection;
@@ -374,44 +345,42 @@ void setnextDirection(struct figure *player)
 	}
 }
 
-
-void printPoints (void) {
-	mvprintw(2,2,"%4d", points);
+void printScore (void) {
+	mvprintw(2,2,"Cookies left:\t%4d", cookies);
+	mvprintw(3,2,"Time:\t\t%4d", seconds);
 	refresh();
-}
-
-void debug (void) {
-	mvprintw(4,4,"%d %d", ghost.x, ghost.y);
 }
 
 int checkIfGameOver(int input) {
 	if ((input == 'x') || (input == 'q')) {
-		return 10;
+		return 20;
 	}
-	if (points > 350) {
+	if (cookies == 0) {
 		return 1;
 	}
-	if (points < 0) {
+	if (seconds < 0) {
 		return 6;
 	}
 	//GAME OVER routine
-	if ( (ghost.x == player.x) && (ghost.y == player.y) ) {
-		return 7;
+	if ((pinky.x == player.x) && (pinky.y == player.y)) {
+		return 11;
 	}
-	if ( (blinky.x == player.x) && (blinky.y == player.y) ) {
-		return 7;
+	if ((blinky.x == player.x) && (blinky.y == player.y)) {
+		return 12;
 	}
-	return 0;
+	if ((bashful.x == player.x) && (bashful.y == player.y)) {
+		return 12;
+	}
+	return 0; // continue playing
 }
 
 int main(void) {
-	int starty,startx;
 	int input;
 
 	initscr();
 	raw();
-	noecho();
-	keypad(stdscr, TRUE);
+	noecho();							// input is not displayed at curser pos
+	keypad(stdscr, TRUE);	// acess to arrow keys
 	curs_set(0);
 
 	//Radom generator initialazation
@@ -420,84 +389,144 @@ int main(void) {
 	start_color();
 	init_pair(1, COLOR_WHITE, COLOR_BLACK);		// Cookies
 	init_pair(2, COLOR_YELLOW, COLOR_BLACK);	// Packman / Player
-	init_pair(3, COLOR_BLUE, COLOR_BLUE);		// Walls
-	init_pair(4, COLOR_MAGENTA, COLOR_BLACK);	// Ghost
-	init_pair(5, COLOR_RED, COLOR_BLACK);		// Blinky
+	init_pair(3, COLOR_BLUE, COLOR_BLUE);			// Walls
+	init_pair(4, COLOR_MAGENTA, COLOR_BLACK);	// Pinky
+	init_pair(5, COLOR_RED, COLOR_BLACK);			// Blinky
+	init_pair(6, COLOR_CYAN, COLOR_BLACK);		// Bashful
 
-	load_maze();
+	load_maze(); // Ititializes the size of the maze
+	cookies = countNumOfCookies();
+	seconds = 100;
 
-	starty = (LINES - sizey) / 2;	/* Calculating for a center placement */
-	startx = (COLS - sizex) / 2;	/* of the window		*/
+	int starty = ((LINES - sizey) / 2);	// Calculating for a center placement
+	int startx = ((COLS - sizex)  / 2); // of the window
+
 	printw("'w', 'a', 's', 'd' to move. 'x' to Exit");
 	refresh();
 
-	win = newwin(sizey, sizex, starty, startx);
+	win = newwin(sizey, sizex, starty, startx);	// opens canvas window in terminal
 	timeout(0);
 
 	// Player starting Position
-	player.y = 5;
-	player.x = 10;
+	player.y = 17;
+	player.x = 11;
 
-	// Ghost starting Position
-	ghost.y = 1;
-	ghost.x = 10;
-    ghost.direction = ' ';
-
-	blinky.y = 1;
+	// Ghosts starting Position
+	blinky.y = 11;
 	blinky.x = 11;
+	blinky.state = 12;
 	blinky.direction = ' ';
 
-	// Timing / Ticks
-	int ticks = 0;
+	pinky.y = 11;
+	pinky.x = 9;
+	pinky.direction = ' ';
+	pinky.state = 16;
 
-	int gameState = 0; // 0 = playing, 1 = won, 7 = defeated by ghost, 10 = terminated
+	bashful.y = 11;
+	bashful.x = 13;
+	bashful.direction = ' ';
+	bashful.state = 16;
+
+	// Timing / Ticks
+	int ticks = 1;
+
+	int gameState = 0; // 0 = playing
 	drawMaze();
 
 	do {
 		input = getch();
-	  	if (input != 255) {
+	  if (input != 255) {
 			setDirectionFromInput(input,&player);
 		}
-		if ((ticks % 30) == 0) {		// evey tenth loop redraws the maze
-			//setDirectionRandom(&ghost);
-			ghostAI(&ghost);
-    		moveFigure(&ghost);
+		if ((ticks % 40) == 0) {
 
-			blinkyAI(&blinky);
-        	moveFigure(&blinky);
-	  }
-	  if ((ticks % 16) == 0) {		// evey tenth loop redraws the maze;
+			// Blinky AI
+			if (blinky.state == 0 ) {
+				ghostAI(&blinky);
+    		moveFigure(&blinky);
+			}
+			else if ((blinky.state > 0) && (blinky.state <= 4)) {
+				stupidAI(&blinky);
+    		moveFigure(&blinky);
+			}
+
+			// Pinky AI
+      if (pinky.state == 0) {
+      	ghostAI(&pinky);
+  			moveFigure(&pinky);
+    	}
+		  else if ((pinky.state > 1) && (pinky.state <= 4)) {
+      	stupidAI(&pinky);
+    		moveFigure(&pinky);
+    	}
+
+			// Bashful AI
+			if (bashful.state == 0) {
+				ghostAI(&bashful);
+				moveFigure(&bashful);
+			}
+			else if ((bashful.state > 1) && (bashful.state <= 4)) {
+				stupidAI(&bashful);
+				moveFigure(&bashful);
+			}
+		}
+
+		// Stats cooldown
+		if ((ticks % 100 == 0) && (blinky.state > 0)) {
+			blinky.state -= 1;
+		}
+		if ((ticks % 100 == 0) && (pinky.state > 0)) {
+			pinky.state -= 1;
+		}
+		if ((ticks % 100 == 0) && (bashful.state > 0)) {
+			bashful.state -= 1;
+		}
+
+		if ((ticks % 16) == 0) {
 			moveFigure(&player);
 			eatCookie(&player);
 			setnextDirection(&player);
 
-			printPoints();
+			printScore();
 			drawMaze();
-	  }
-		if ((ticks % 100) == 0) {
-			//points = points - 5;
-			printPoints();
 		}
+		if ((ticks % 110) == 0) {
+			seconds -= 1;
+			printScore();
+		}
+
 		gameState = checkIfGameOver(input);
-		usleep(10000);
+		usleep(7000);
 	  ticks++;
 	} while (!gameState);
 
 	if (gameState == 1) {
-		mvprintw(5,29,"CONGRATS - YOU WON!");
+		mvprintw(3,23,"***** - CONGRATULATIONS! - *****");
+		mvprintw(4,23,"======  - YOU'VE WON! -  =======");
   }
 	else if (gameState == 6) {
-		mvprintw(5,29,"YOU LOST ALL  POINTS");
+		mvprintw(3,23,"      YOU LOST - TIME'S UP      ");
   }
-	else if (gameState == 7) {
-		mvprintw(5,29,"YOU LOST - GAME OVER");
+	else if (gameState == 11) {
+		mvprintw(3,23,"********* YOU'VE LOST **********");
+		mvprintw(4,23,"======  CAUGTH BY PINKY  =======");
 	}
-	else if (gameState == 10) {
-		mvprintw(5,29,"QUIT: GAME TERMINATED");
+	else if (gameState == 12) {
+		mvprintw(3,23,"********* YOU'VE LOST **********");
+		mvprintw(4,23,"======  CAUGTH BY BLINKY  ======");
+	}
+	else if (gameState == 13) {
+		mvprintw(3,23,"********* YOU'VE LOST **********");
+		mvprintw(4,23,"=====  CAUGTH BY BASHFUL  ======");
+	}
+	else if (gameState == 20) {
+		mvprintw(3,23,"      QUIT: GAME TERMINATED     ");
   }
 	refresh();
-	sleep(2);
+
+	sleep(3);
 	delwin(win);
 	endwin();
+
 	return 0;
 }
